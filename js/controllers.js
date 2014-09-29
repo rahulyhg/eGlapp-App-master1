b77a548e61febab01a8b5a2b98f46a43fe7f5800
jagruti181 <patiljagruti181@gmail.com>
2014-09-29 19:02:21 +0530

    b

--- js/controllers.js
+++ js/controllers.js
@@ -325,7 +325,7 @@ angular.module('starter.controllers', ['restservicemod','angularFileUpload','ngC
         });
 
         //Upload photo
-        var server = 'http://wohlig.biz/Toykraftbackend/index.php/json/uploadfile';
+        var server = 'http://digitalmindsinc.co/eglapp11/admin/index.php/event/filee';
 
         //File Upload parameters: source, filePath, options
         $scope.uploadPhoto = function () {
